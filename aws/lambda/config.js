b92e061e49ab4bc2038c1a18c3251f0fe31f44f5
André Alencar <20287544+alencarandre@users.noreply.github.com>
2019-03-27 01:24:28 -0300

    Lambda sqsSeed refactory (#3)
    - Organize lambda parameters
      - Separate config into config.js file
    - Splice in functions among resposability
    - Check if request is valid
    - Set accessed_at field with current timestamp

---
+++ aws/lambda/config.js
@@ -0,0 +1,9 @@
+const REGION = 'sa-east-1';
+const AWS_ACCOUNT_ID = '999999999999';
+const QUEUE_NAME = 'queue-name';
+const QUEUE_URL = 'https://sqs.' + REGION + '.amazonaws.com/' + AWS_ACCOUNT_ID + '/' + QUEUE_NAME;
+
+module.exports {
+  region: REGION,
+  queue_url: QUEUE_URL
+}
--- aws/lambda/sqsseed.js
+++ aws/lambda/sqsseed.js
@@ -1,25 +1,43 @@
 const AWS = require('aws-sdk');
+const config = require('./config')
 
-const REGION = 'sa-east-1';
-const AWS_ACCOUNT_ID = '999999999999';
-const QUEUE_NAME = 'queue-name';
-const QUEUE_URL = 'https://sqs.' + REGION + '.amazonaws.com/' + AWS_ACCOUNT_ID + '/' + QUEUE_NAME;
+const sqs = new AWS.SQS({ region : config.region });
 
-const sqs = new AWS.SQS({ region : REGION });
+const validEvent = function(event) {
+  return event.guid && event.location
+}
 
-exports.handler = function(event, context) {
-  const params = {
-    MessageBody: JSON.stringify(event),
-    QueueUrl: QUEUE_URL
-  };
+const queueData = function(event) {
+  return {
+    guid: event.guid,
+    location: event.location,
+    accessed_at: new Date().getTime()
+  }
+}
 
+const queueParams = function(event, queueUrl) {
+  return {
+    MessageBody: JSON.stringify(queueData(event)),
+    QueueUrl: queueUrl
+  }
+}
+
+const sendMessage = function(params, context) {
   sqs.sendMessage(params, function(err, data) {
     if(err) {
       console.log('error:',"Fail Send Message" + err);
       context.done('error', "ERROR Put SQS");
-    } else{
+    } else {
       console.log('data:', data.MessageId);
       context.done(null, '');
     }
   });
+}
+
+exports.handler = function(event, context) {
+  if(validEvent(event)) {
+    sendMessage(queueParams(event, config.queue_url), context)
+  } else {
+    context.done('error', 'Invalid event')
+  }
 }
