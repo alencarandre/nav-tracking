44b1f92a09c9b910168becd61530bcb3999ec220
André Alencar <andreluis88@gmail.com>
2019-03-26 22:27:37 -0300

    Install webpack dev server

--- nav-traking-lib-js/webpack.config.js
+++ nav-traking-lib-js/webpack.config.js
@@ -2,5 +2,10 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "./nav-traking.js"
+  },
+  devServer: {
+    contentBase: './dist',
+    compress: true,
+    port: 9000
   }
 }
