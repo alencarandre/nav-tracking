a1e4e62392191125c6112ed55573701fca4cdd91
André Alencar <andreluis88@gmail.com>
2019-03-31 05:14:11 -0300

    Page contact
    This page get and use NavTracking api in order to save contact

---
+++ page-sample/js/contact.js
@@ -0,0 +1,24 @@
+(function(navTracking) {
+  const onSubmit = function(ev) {
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    const name = $('form#contact input#name').val();
+    const email = $('form#contact input#email').val();
+
+    debugger
+
+    navTracking
+      .contact
+      .submit(name, email)
+      .then(function(contact) {
+        toastr.success('Success on send your contact. Thank you!');
+      }).catch(function(message) {
+        toastr.error(message.error);
+      })
+
+    return false;
+  }
+
+  $(document).on('submit', 'form#contact', onSubmit)
+})(window.NavTracking);
