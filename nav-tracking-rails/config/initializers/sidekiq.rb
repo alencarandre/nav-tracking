8e034736a51e022236daff8c1d400d286324fbf6
André Alencar <andreluis88@gmail.com>
2019-04-01 02:02:44 -0300

    Docker for sidekiq

--- nav-tracking-rails/config/initializers/sidekiq.rb
+++ nav-tracking-rails/config/initializers/sidekiq.rb
@@ -1,11 +1,13 @@
 Sidekiq.configure_server do |config|
   config.average_scheduled_poll_interval = 15
 end
-# 
-# Sidekiq.configure_server do |config|
-#   config.redis = {url: 'redis://localhost:6379/0', network_timeout: 5}
-# end
-#
-# Sidekiq.configure_client do |config|
-#   config.redis = {url: 'redis://localhost:6379/0', network_timeout: 5}
-# end
+
+redis_server = ENV['REDISHOST'] || 'redis://localhost:6379/0'
+
+Sidekiq.configure_server do |config|
+  config.redis = {url: redis_server, network_timeout: 5}
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {url: redis_server, network_timeout: 5}
+end
